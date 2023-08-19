50d1d5b4bbfe39b27b192812a47cf0f9dff907a8
maawoo <marco.wolsza@uni-jena.de>
2023-08-19 15:19:06 +0200

    new module load.py

---
+++ gedixr/load.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import geopandas as gp
+
+
+def gpkg_to_gdf(gpkg_l2a=None, gpkg_l2b=None):
+    """
+    Loads GEDI L2A and/or L2B Geopackage files as GeoDataFrames. If both are provided, they will be merged into a single
+    GeoDataFrame.
+
+    Parameters
+    ----------
+    gpkg_l2a: str|Path, optional
+        Path to a GEDI L2A Geopackage file.
+    gpkg_l2b: str|Path, optional
+        Path to a GEDI L2B Geopackage file.
+
+    Returns
+    -------
+    final_gdf: geopandas.GeoDataFrame
+        GeoDataFrame containing the data from the provided GEDI L2A and/or L2B Geopackage files.
+    """
+    if all(x is None for x in [gpkg_l2a, gpkg_l2b]):
+        raise RuntimeError("At least one of the parameters 'gpkg_l2a' or 'gpkg_l2b' must be provided!")
+    elif all(x is not None for x in [gpkg_l2a, gpkg_l2b]):
+        gdf_l2a = gp.read_file(gpkg_l2a)
+        gdf_l2b = gp.read_file(gpkg_l2b)
+        final_gdf = merge_gdf(gdf_l2a=gdf_l2a, gdf_l2b=gdf_l2b)
+    else:
+        gpkg = gpkg_l2a if gpkg_l2a is not None else gpkg_l2b
+        final_gdf = gp.read_file(gpkg)
+        final_gdf['acq_time'] = pd.to_datetime(final_gdf['acq_time'])
+    return final_gdf
+
+
+def merge_gdf(gdf_l2a, gdf_l2b):
+    """
+    Merges two GEDI L2A and L2B GeoDataFrames on their `geometry` column.
+
+    Parameters
+    ----------
+    gdf_l2a: geopandas.GeoDataFrame
+        GeoDataFrame containing GEDI L2A data.
+    gdf_l2b: geopandas.GeoDataFrame
+        GeoDataFrame containing GEDI L2B data.
+
+    Returns
+    -------
+    merged_gdf: geopandas.GeoDataFrame
+        GeoDataFrame containing the data from the provided GEDI L2A and L2B GeoDataFrames.
+    """
+    if len(gdf_l2a) != len(gdf_l2b):
+        print(f"WARNING: The GEDI L2A and L2B GeoDataFrames have different number of rows "
+              f"({len(gdf_l2a)} vs. {len(gdf_l2b)})."
+              f"\nThey will be merged on their geometry column, which may lead to unexpected results and/or missing "
+              f"data.")
+    gdf_l2a = gdf_l2a.loc[:, ['rh98', 'geometry']]
+    merged_gdf = gdf_l2b.merge(gdf_l2a, how='inner', on='geometry')
+    merged_gdf['acq_time'] = pd.to_datetime(merged_gdf['acq_time'])
+    return merged_gdf
