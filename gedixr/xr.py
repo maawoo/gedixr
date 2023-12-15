b8805e94ac936ebe4025d226f61420b632eefbcf
maawoo <marco.wolsza@uni-jena.de>
2023-12-15 11:59:15 +0100

    [xr] improve typing; linting

--- gedixr/xr.py
+++ gedixr/xr.py
@@ -2,26 +2,35 @@ import pandas as pd
 import geopandas as gp
 from geocube.api.core import make_geocube
 
+from typing import Optional
+from pathlib import Path
+from geopandas import GeoDataFrame
+from xarray import Dataset
 
-def gpkg_to_gdf(gpkg_l2a=None, gpkg_l2b=None):
+
+def gpkg_to_gdf(gpkg_l2a: Optional[str | Path] = None,
+                gpkg_l2b: Optional[str | Path] = None
+                ) -> GeoDataFrame:
     """
-    Loads GEDI L2A and/or L2B Geopackage files as GeoDataFrames. If both are provided, they will be merged into a single
-    GeoDataFrame.
-
+    Loads GEDI L2A and/or L2B Geopackage files as GeoDataFrames. If both are
+    provided, they will be merged into a single GeoDataFrame.
+    
     Parameters
     ----------
-    gpkg_l2a: str|Path, optional
+    gpkg_l2a: str or Path, optional
         Path to a GEDI L2A Geopackage file.
-    gpkg_l2b: str|Path, optional
+    gpkg_l2b: str or Path, optional
         Path to a GEDI L2B Geopackage file.
-
+    
     Returns
     -------
-    final_gdf: geopandas.GeoDataFrame
-        GeoDataFrame containing the data from the provided GEDI L2A and/or L2B Geopackage files.
+    final_gdf: GeoDataFrame
+        GeoDataFrame containing the data from the provided GEDI L2A and/or L2B
+        Geopackage files.
     """
     if all(x is None for x in [gpkg_l2a, gpkg_l2b]):
-        raise RuntimeError("At least one of the parameters 'gpkg_l2a' or 'gpkg_l2b' must be provided!")
+        raise RuntimeError("At least one of the parameters 'gpkg_l2a' or "
+                           "'gpkg_l2b' must be provided!")
     elif all(x is not None for x in [gpkg_l2a, gpkg_l2b]):
         gdf_l2a = gp.read_file(gpkg_l2a)
         gdf_l2b = gp.read_file(gpkg_l2b)
@@ -33,55 +42,65 @@ def gpkg_to_gdf(gpkg_l2a=None, gpkg_l2b=None):
     return final_gdf
 
 
-def merge_gdf(gdf_l2a, gdf_l2b):
+def merge_gdf(gdf_l2a: GeoDataFrame,
+              gdf_l2b: GeoDataFrame
+              ) -> GeoDataFrame:
     """
     Merges two GEDI L2A and L2B GeoDataFrames on their `geometry` column.
-
+    
     Parameters
     ----------
-    gdf_l2a: geopandas.GeoDataFrame
+    gdf_l2a: GeoDataFrame
         GeoDataFrame containing GEDI L2A data.
-    gdf_l2b: geopandas.GeoDataFrame
+    gdf_l2b: GeoDataFrame
         GeoDataFrame containing GEDI L2B data.
-
+    
     Returns
     -------
-    merged_gdf: geopandas.GeoDataFrame
-        GeoDataFrame containing the data from the provided GEDI L2A and L2B GeoDataFrames.
+    merged_gdf: GeoDataFrame
+        GeoDataFrame containing the data from the provided GEDI L2A and L2B
+        GeoDataFrames.
     """
     if len(gdf_l2a) != len(gdf_l2b):
-        print(f"WARNING: The GEDI L2A and L2B GeoDataFrames have different number of rows "
+        print(f"WARNING: The GEDI L2A and L2B GeoDataFrames have different "
+              f"number of rows "
               f"({len(gdf_l2a)} vs. {len(gdf_l2b)})."
-              f"\nThey will be merged on their geometry column, which may lead to unexpected results and/or missing "
-              f"data.")
+              f"\nThey will be merged on their geometry column, which may lead "
+              f"to unexpected results and/or missing data.")
     gdf_l2a = gdf_l2a.loc[:, ['rh98', 'geometry']]
     merged_gdf = gdf_l2b.merge(gdf_l2a, how='inner', on='geometry')
     merged_gdf['acq_time'] = pd.to_datetime(merged_gdf['acq_time'])
     return merged_gdf
 
 
-def gdf_to_xr(gdf, gedi_vars=None, resolution=None):
+def gdf_to_xr(gdf: GeoDataFrame,
+              gedi_vars: Optional[list[str]] = None,
+              resolution: Optional[tuple[float, float]] = None
+              ) -> Dataset:
     """
     Rasterizes a GeoDataFrame containing GEDI L2A/L2B data to an xarray Dataset.
     
     Parameters
     ----------
-    gdf: geopandas.GeoDataFrame
+    gdf: GeoDataFrame
         GeoDataFrame containing GEDI L2A/L2B data.
-    gedi_vars: list(str)
-        List of attribute names (i.e. GEDI variables) to be included. Defaault is None, which will include all
-        variables.
-    resolution:
-        A tuple of the pixel spacing of the returned data (Y, X). This includes the direction
-        (as indicated by a positive or negative number). Default is (-0.0003, 0.0003), which corresponds to a spacing
-        of 30 m.
+    gedi_vars: list of str, optional
+        List of attribute names (i.e. GEDI variables) to be included.
+        Default is None, which will include all variables.
+    resolution: tuple of float, optional
+        A tuple of the pixel spacing of the returned data (Y, X). This includes
+        the direction (as indicated by a positive or negative number). Default
+        is (-0.0003, 0.0003), which corresponds to a spacing of 30 m.
     
     Returns
     -------
-    cube: xarray.Dataset
+    cube: Dataset
         An xarray Dataset containing the rasterized GEDI data.
     """
     if resolution is None:
         resolution = (-0.0003, 0.0003)
-    xr_ds = make_geocube(gdf, measurements=gedi_vars, output_crs=f'epsg:{gdf.crs.to_epsg()}', resolution=resolution)
+    xr_ds = make_geocube(vector_data=gdf,
+                         measurements=gedi_vars,
+                         output_crs=f'epsg:{gdf.crs.to_epsg()}',
+                         resolution=resolution)
     return xr_ds
